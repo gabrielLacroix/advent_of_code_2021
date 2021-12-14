fc24a8199568c8a40be95a3cb90c3acef47e0998
GabrielLacroix <gabriel.lacroix2@usherbrooke.ca>
2021-12-13 22:09:51 -0500

    Day 2 challenge 1

---
+++ python/day_2/dive/Part1.py
@@ -0,0 +1,35 @@
+if __name__ == '__main__':
+
+    total_depth = 0
+    total_position = 0
+
+    def forward(horizontal_shift):
+        global total_position
+        total_position += horizontal_shift
+
+    def dive(depth):
+        global total_depth
+        total_depth += depth
+
+    funcs = [('forward', lambda i: forward(i)),
+             ('up', lambda i: dive(-i)),
+             ('down', lambda i: dive(i))]
+
+    def get_func(action):
+        return get_action(action)[1]
+
+    def get_action(action):
+        for i, (action_name, func) in enumerate(funcs):
+            if action_name == action:
+                return funcs[i]
+
+    data = open('input').read().split('\n')
+    for line in data:
+        action_amount = line.split(' ')
+        if len(action_amount) == 2:
+            action, x = action_amount
+            get_func(action)(int(x))
+
+    print("Depth = " + str(total_depth))
+    print("Position = " + str(total_position))
+    print("Result = " + str(total_depth * total_position))
