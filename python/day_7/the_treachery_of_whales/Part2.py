a39b15613e347ea5aceb0faf1c0570811a79dbb8
GabrielLacroix <gabriel.lacroix2@usherbrooke.ca>
2021-12-14 20:22:29 -0500

    Day 7 challenge 2

---
+++ python/day_7/the_treachery_of_whales/Part2.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    data = open('input').read().split(',')
+    data = [int(f) for f in data]
+
+    fuel_counts = []
+    for possible_index in range(max(data)):
+        fuel_count = 0
+        for crab in data:
+            n = abs(crab - possible_index)
+            fuel_count += n * (n + 1) // 2
+
+        fuel_counts.append(fuel_count)
+    print(min(fuel_counts))
