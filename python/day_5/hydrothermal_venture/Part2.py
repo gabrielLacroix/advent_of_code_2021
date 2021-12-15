c465414d478c654e50f5f43825ea3dfec5ff7155
GabrielLacroix <gabriel.lacroix2@usherbrooke.ca>
2021-12-14 19:30:19 -0500

    Day 5 challenge 2

---
+++ python/day_5/hydrothermal_venture/Part2.py
@@ -0,0 +1,75 @@
+if __name__ == '__main__':
+    data = open('input').read().split('\n')
+    data.remove('')
+
+    def get_values(line):
+        split = line.split()
+        split.remove('->')
+        values = []
+        if is_horizontal(split):
+
+            p1, p2 = split
+            x1, y = p1.split(',')
+            x2, _ = p2.split(',')
+            x1, x2 = int(x1), int(x2)
+            x1, x2 = min_max(x1, x2)
+            for x in range(int(x1), int(x2) + 1):
+                values.append((x, int(y)))
+            pass
+        elif is_vertical(split):
+            p1, p2 = split
+            x, y1 = p1.split(',')
+            _, y2 = p2.split(',')
+            y1, y2 = int(y1), int(y2)
+            y1, y2 = min_max(y1, y2)
+            for y in range(int(y1), int(y2) + 1):
+                values.append((int(x), y))
+            pass
+        else:
+            p1, p2 = split
+            x1, y1 = p1.split(',')
+            x2, y2 = p2.split(',')
+            x_inc = 1 if int(x1) < int(x2) else -1
+            y_inc = 1 if int(y1) < int(y2) else -1
+            x_iter = int(x1)
+            y_iter = int(y1)
+            values.append((x_iter, y_iter))
+            while x_iter != int(x2) and y_iter != int(y2):
+                x_iter += x_inc
+                y_iter += y_inc
+                values.append((x_iter, y_iter))
+
+            pass
+
+        return values
+
+    def min_max(a, b):
+        if a < b:
+            return a, b
+        return b, a
+
+    def is_vertical(line):
+        p1, p2 = line
+        x1, _ = p1.split(',')
+        x2, _ = p2.split(',')
+        return x1 == x2
+
+    def is_horizontal(line):
+        p1, p2 = line
+        _, y1 = p1.split(',')
+        _, y2 = p2.split(',')
+        return y1 == y2
+
+    list_of_2s = set()
+    list_of_1s = set()
+
+    for entry in data:
+        values = get_values(entry)
+        for point in values:
+            if point in list_of_1s:
+                list_of_2s.add(point)
+            else:
+                list_of_1s.add(point)
+
+    print(len(list_of_2s))
+
