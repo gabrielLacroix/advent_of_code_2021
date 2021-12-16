fb635f1fda92e9e578f56e98aa0f93b46ff89407
GabrielLacroix <gabriel.lacroix2@usherbrooke.ca>
2021-12-16 17:53:32 -0500

    Day 10 challenge 1

---
+++ python/day_10/syntax_scoring/Part1.py
@@ -0,0 +1,29 @@
+if __name__ == '__main__':
+    data = open('input').read().split('\n')
+    data.remove('')
+    print(data)
+
+    openers = ['(', '[', '{', '<']
+    closers = [')', ']', '}', '>']
+
+    illegal_character_scores = {')': 3, ']': 57, '}': 1197, '>': 25137, '': 0}
+    opener_to_closer = {'(': ')', '[': ']', '{': '}', '<': '>'}
+
+    def find_first_illegal_character(source):
+        expected_closer = []
+        for c in source:
+            if c in openers:
+                expected_closer.append(opener_to_closer.get(c))
+            else:
+                closer = expected_closer.pop()
+                if c != closer:
+                    return c
+
+        return ''
+
+    score = 0
+    for line in data:
+
+        first_illegal_character = find_first_illegal_character(line)
+        score += illegal_character_scores.get(first_illegal_character)
+    print(score)
